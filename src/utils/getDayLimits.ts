161d36150dda6c3b6eed30c1cc147878ed5ee0df
emanuelcardozo <leuname.01@gmail.com>
2024-11-21 09:47:49 -0300

    Changing getDayLimits from Date api to momentjs

--- src/utils/getDayLimits.ts
+++ src/utils/getDayLimits.ts
@@ -1,12 +1,8 @@
+import moment from 'moment'
+
 export default function getDayLimits(): { startOfDay: Date; endOfDay: Date } {
-  const startOfDay = new Date()
-  startOfDay.setUTCHours(0, 0, 0, 0)
-
-  const endOfDay = new Date()
-  endOfDay.setUTCHours(23, 59, 59, 59)
-
   return {
-    startOfDay,
-    endOfDay,
+    startOfDay: moment().startOf('date').toDate(),
+    endOfDay: moment().endOf('date').toDate(),
   }
 }
