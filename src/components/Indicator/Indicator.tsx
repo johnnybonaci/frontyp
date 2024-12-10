4f55d473bbb723f7acd13397e2719d30444edc96
emanuelcardozo <leuname.01@gmail.com>
2024-12-10 18:15:04 -0300

    Adding double indicator

--- src/components/Indicator/Indicator.tsx
+++ src/components/Indicator/Indicator.tsx
@@ -1,16 +1,17 @@
 import { type FC } from 'react'
 import styles from './indicator.module.scss'
-import { Grow, Skeleton } from '@mui/material'
+import { Grow, Skeleton, Stack, Typography } from '@mui/material'
 import GrowthIndicator from 'src/components/GrowthIndicator'
-import { type IndicatorSettings } from 'hooks/useTableSettings.tsx'
+import { type DoubleIndicatorSettings, type IndicatorSettings } from 'hooks/useTableSettings.tsx'
+import { DOUBLE_INDICATOR } from 'utils/constants'
 
 export interface IndicatorProps {
-  indicator: IndicatorSettings
+  indicator: IndicatorSettings | DoubleIndicatorSettings
   loading?: boolean
 }
 
 const Indicator: FC<IndicatorProps> = ({ indicator, loading }) => {
-  const { name, value, growthPercentage } = indicator
+  const { growthPercentage } = indicator
 
   if (loading) {
     return (
@@ -23,6 +24,36 @@ const Indicator: FC<IndicatorProps> = ({ indicator, loading }) => {
       </div>
     )
   }
+
+  if (indicator.type === DOUBLE_INDICATOR) {
+    const { name, values } = indicator as DoubleIndicatorSettings
+
+    return (
+      <Grow in>
+        <div className={styles.indicator}>
+          <Stack alignItems="center">
+            <Typography fontSize={14}>{name}</Typography>
+            <Stack direction="row" width="100%" justifyContent="space-around">
+              <Stack alignItems="center">
+                <Typography fontSize={14}>{values[0].name}</Typography>
+                <Typography fontSize={20} variant="caption">
+                  {values[0].value}
+                </Typography>
+              </Stack>
+              <Stack alignItems="center">
+                <Typography fontSize={14}>{values[1].name}</Typography>
+                <Typography fontSize={20} variant="caption">
+                  {values[1].value}
+                </Typography>
+              </Stack>
+            </Stack>
+          </Stack>
+        </div>
+      </Grow>
+    )
+  }
+
+  const { name, value } = indicator as IndicatorSettings
 
   return (
     <Grow in>
