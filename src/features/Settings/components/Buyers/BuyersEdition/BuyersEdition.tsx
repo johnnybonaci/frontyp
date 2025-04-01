4c0d498692f55c45e32d911f0c09356a5df1dcf6
Johnny Jose <johnnybonaci@gmail.com>
2025-04-01 15:56:58 -0300

    fix

--- src/features/Settings/components/Buyers/BuyersEdition/BuyersEdition.tsx
+++ src/features/Settings/components/Buyers/BuyersEdition/BuyersEdition.tsx
@@ -56,6 +56,7 @@ function BuyersEdition({
       resetForm({
         values: buyersToForm(buyers),
       })
+      console.log('buyers', buyers)
       setFieldValue('provider', buyers.provider)
     }
   }, [buyers])
