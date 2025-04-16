a562aa2444ec67bc1506f30400de38ac53fa818c
Johnny Jose <johnnybonaci@gmail.com>
2025-04-16 13:17:27 -0300

    Refactor dateFromUrl to return the start of the day for accurate date handling and enhance logging in getDayLimits

--- src/utils/dateFromUrl.ts
+++ src/utils/dateFromUrl.ts
@@ -2,7 +2,8 @@ import moment from 'moment-timezone'
 import { DEFAULT_DATE_TIMEZONE } from 'utils/constants.ts'
 
 function dateFromUrl(date: string, timezone: string = DEFAULT_DATE_TIMEZONE): Date {
-  return moment.tz(date, timezone).toDate()
+  return moment.tz(date, timezone).startOf('day').toDate()
+
 }
 
 export default dateFromUrl
--- src/utils/getDayLimits.ts
+++ src/utils/getDayLimits.ts
@@ -1,6 +1,7 @@
 import currentDate from 'utils/currentDate.ts'
 
 export default function getDayLimits(): { startOfDay: Date; endOfDay: Date } {
+  console.log('getDayLimits ->' + currentDate())
   return {
     startOfDay: currentDate(),
     endOfDay: currentDate(),
