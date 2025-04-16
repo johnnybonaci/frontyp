f7a41afbc2da61f1dc78560b9424948488d2cba7
Johnny Jose <johnnybonaci@gmail.com>
2025-04-16 14:14:23 -0300

    Refactor main.tsx to remove React.StrictMode wrapper for cleaner rendering

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react'
+import { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import i18n from 'src/i18n.ts'
@@ -6,11 +6,9 @@ import { I18nextProvider } from 'react-i18next'
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <I18nextProvider i18n={i18n} defaultNS={'translation'}>
-      <Suspense>
-        <App />
-      </Suspense>
-    </I18nextProvider>
-  </React.StrictMode>
+  <I18nextProvider i18n={i18n} defaultNS={'translation'}>
+    <Suspense>
+      <App />
+    </Suspense>
+  </I18nextProvider>
 )
